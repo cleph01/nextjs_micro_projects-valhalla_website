02c305a0ff6c94785b691b601073d59eb0968d4b
Charlie <charles.j.montoya@gmail.com>
2023-03-21 23:10:10 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Manrope } from "next/font/google";
+
+const manrope = Manrope({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div className={manrope.className}>
+            <Component {...pageProps} />
+        </div>
+    );
 }
